89ba78fba2acb1df2ad8fce62a24746d9278db48
Viza Von <>
2025-02-28 10:33:37 +0700

    fix filament

--- resources/views/filament/components/topbar.blade.php
+++ resources/views/filament/components/topbar.blade.php
@@ -3,7 +3,7 @@
     <div class="flex space-x-6 items-center">
         <div class="relative w-[255px]">
             <input type="text" placeholder="Search for something"
-                class="text-[#8BA3CB] w-full h-[50px] border-none bg-[#F5F7FA] rounded-full px-12 outline-none">
+                class="text-[#8BA3CB] w-full h-[50px] text-[15px] border-none bg-[#F5F7FA] rounded-full px-12 outline-none">
             <img src="/svg/search.svg" alt="Search Icon" class="absolute left-5 top-1/2 transform -translate-y-1/2 w-5 h-5">
         </div>
         <div class="bg-[#F5F7FA] hover:border hover:scale-110 transition-all duration-300 hover:border-blue-400 cursor-pointer w-[50px] h-[50px] flex items-center justify-center rounded-full">
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                // Add Filament theme
                 'resources/css/filament/admin/theme.css'
             ],
             refresh: [
